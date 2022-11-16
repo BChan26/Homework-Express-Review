6b54d9b7dc4730cd271cc794a25d4d6c7a2d6c9b
Brandon Chan <bchan26@bu.edu>
2022-11-16 14:26:58 -0500

    included comments to be clearer about what lines of code are and what they are doing

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 //http://localhost:3001/origin
-//ALL data of origin
+//Route #1
+//displays EVERYTHING of origin.js
 // app.get('/origin', (request, response) => {
 //     response.send(origin);
 // })
 
-//origin's type of transportation (bus)
+//Info with at least 5 pieces of data to get
 app.get('/origin', (request, response) => {
     response.send(`This mode of transportation is a ${origin.type}. This is route #${origin.route}. This stop is at ${origin.stopName}. The stop ID is ${origin.stopID} and it's stop #${origin.stopNumber} on the route.`);
 })
 
 //http://localhost:3001/end
-//entire object of end
+//Route #2
+//displays EVERYTHING of end.js
 // app.get('/end', (request, response) => {
 //     response.send(end);
 // })
 
-//origin's type of transportation (bus)
+//Info with at least 5 pieces of data to get
 app.get('/end', (request, response) => {
     response.send(`This mode of transportation is a ${end.type}. This is route #${end.route}. This stop is at ${end.stopName}. The stop ID is ${end.stopID} and it's stop #${end.stopNumber} on the route.`);
 })
